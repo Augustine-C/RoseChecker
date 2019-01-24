8629d04877da3d7ca06a453c9516d1ef0e77605b
Yifan Cui <zjgdxbs@me.com>
2019-01-23 21:56:30 -0500

    merge work

--- app/src/main/java/edu/rosehulman/cuiy1/rosechecker/ScheduleViewHolder.kt
+++ app/src/main/java/edu/rosehulman/cuiy1/rosechecker/ScheduleViewHolder.kt
@@ -2,13 +2,24 @@ package edu.rosehulman.cuiy1.rosechecker
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import kotlinx.android.synthetic.main.event_holder.view.*
 
-class ScheduleViewHolder(itemView:View,adapter: ScheduleAdapter):RecyclerView.ViewHolder(itemView) {
+class ScheduleViewHolder(itemView:View, val adapter: ScheduleAdapter):RecyclerView.ViewHolder(itemView) {
     init {
         //TODO Set On Edit Listener
+        itemView.setOnClickListener{
+            adapter
+        }
     }
 
-    fun bind(){
-        //TODO
+    fun bind(event: Event){
+        itemView.eventName.text = event.name
+        itemView.event_time.text = event.time.toString()
+        itemView.event_location.text = event.location
+    }
+
+    fun processTime(time : Int) : String{
+        //TODO to be implemented
+        return ""
     }
 }
