8e8aa5637397a11e00e87236c4035c2962feb798
tigerchensh <tigersh@Outlook.com>
2019-01-29 15:33:13 -0500

    edit and remove Work

--- app/src/main/java/edu/rosehulman/cuiy1/rosechecker/ScheduleAdapter.kt
+++ app/src/main/java/edu/rosehulman/cuiy1/rosechecker/ScheduleAdapter.kt
@@ -12,22 +12,24 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.add_course_event.view.*
+import kotlinx.android.synthetic.main.add_meeting_event.view.*
 import java.text.DateFormat
 import java.util.*
 
-class ScheduleAdapter(var context: Context?, var date : Date) : RecyclerView.Adapter<ScheduleViewHolder>() {
+class ScheduleAdapter(var context: Context?, var date: Date) : RecyclerView.Adapter<ScheduleViewHolder>() {
 
     var events = ArrayList<Event>()
     lateinit var registration: ListenerRegistration
     private val eventsRef = FirebaseFirestore
         .getInstance()
         .collection(Constants.EVENTS_COLLECTION)
+
     fun addSnapshotListener() {
         Log.d("!!!", "add snapshotlistener ${events}")
 //        PicListWrapper.picList = ArrayList()
         registration = eventsRef
-            .addSnapshotListener{ snapshot : QuerySnapshot?, firebaseFirestoreException ->
-                if(firebaseFirestoreException != null){
+            .addSnapshotListener { snapshot: QuerySnapshot?, firebaseFirestoreException ->
+                if (firebaseFirestoreException != null) {
                     Log.w("!!!", "Firebase Error: $firebaseFirestoreException")
                     return@addSnapshotListener
                 }
@@ -38,12 +40,12 @@ class ScheduleAdapter(var context: Context?, var date : Date) : RecyclerView.Ada
     }
 
     private fun processSnapshotDiff(snapshot: QuerySnapshot) {
-        for(documentChange in snapshot.documentChanges){
+        for (documentChange in snapshot.documentChanges) {
             val event = Event.fromSnapshot(documentChange.document)
-            when (documentChange.type){
+            when (documentChange.type) {
                 DocumentChange.Type.ADDED -> {
                     Log.d("!!!", "ADDED")
-                    if(event.timestamp.equals("${date.year}${date.month}${date.date}")) {
+                    if (event.timestamp.equals("${date.year}${date.month}${date.date}")) {
                         events.add(0, event)
                         notifyItemInserted(0)
                     }
@@ -56,7 +58,7 @@ class ScheduleAdapter(var context: Context?, var date : Date) : RecyclerView.Ada
                 }
                 DocumentChange.Type.MODIFIED -> {
                     val pos = events.indexOfFirst { it.id == event.id }
-                    Log.d("!!!","MODIFY"+pos.toString())
+                    Log.d("!!!", "MODIFY" + pos.toString())
                     events[pos] = event
                     notifyItemChanged(pos)
                 }
@@ -75,40 +77,83 @@ class ScheduleAdapter(var context: Context?, var date : Date) : RecyclerView.Ada
     }
 
     fun showEditDialog(position: Int) {
-        val builder = AlertDialog.Builder(this.context!!)
-        //configure build: title icon message or custom view or list. Buttons (pos, neg, neutral)
-        builder.setTitle("Edit a dialog")
-        val view = LayoutInflater.from(context).inflate(R.layout.add_course_event, null, false)
-        builder.setView(view)
-        val event = events[position]
-        when (event.javaClass) {
 
+        val event = events[position]
+        when (event.eventType) {
+            Event.EventType.NomalEvent -> {
+
+            }
+            Event.EventType.MeetingEvent -> {
+                val builder = AlertDialog.Builder(this.context!!)
+                builder.setTitle("Edit a Meeting Event")
+                val view = LayoutInflater.from(context).inflate(R.layout.add_meeting_event, null, false)
+                view.title.setText(event.name)
+                view.location.setText(event.location)
+                view.meeting_agenda.setText(event.meetingInfo.get("meetingAgenda"))
+                view.meeting_member.setText(event.meetingInfo.get("meetingMember"))
+                builder.setView(view)
+                builder.setPositiveButton(android.R.string.ok, { _, _ ->
+                    val name = view.title.text.toString()
+                    val location = view.location.text.toString()
+                    val meeting_agenda = view.meeting_agenda.text.toString()
+                    val meeting_member = view.meeting_member.text.toString()
+                    editMeeting(position, name, location, meeting_agenda, meeting_member)
+                })
+                builder.setNeutralButton("Remove") { _, _ ->
+                           remove(position)
+                }
+                builder.create().show()
+            }
+            Event.EventType.CourseEvent -> {
+                val builder = AlertDialog.Builder(this.context!!)
+                builder.setTitle("Edit a Coure Event")
+                val view = LayoutInflater.from(context).inflate(R.layout.add_course_event, null, false)
+                view.title.setText(event.name)
+                view.location.setText(event.location)
+                view.keycontent.setText(event.courseInfo.get("keyContent"))
+                view.homework.setText(event.courseInfo.get("homeWork"))
+                builder.setView(view)
+                builder.setPositiveButton(android.R.string.ok, { _, _ ->
+                    val name = view.title.text.toString()
+                    val location = view.location.text.toString()
+                    val keyContent = view.keycontent.text.toString()
+                    val homework = view.homework.text.toString()
+                    editCourse(position, name, location, keyContent, homework)
+                })
+                builder.setNeutralButton("Remove") { _, _ ->
+                    remove(position)
+                }
+                builder.create().show()
+            }
             else -> {
                 true
             }
         }
 
-        builder.setPositiveButton(android.R.string.ok, { _, _ ->
-            // edit(position,caption,url)
-        })
-        builder.setNeutralButton("Remove") { _, _ ->
-            //            remove(position)
-        }
-        builder.create().show()
+
     }
 
-    fun add(event: Event) {
-        eventsRef.add(event)
+    fun editMeeting(position: Int, name: String, location: String, meeting_agenda: String, meeting_member: String) {
+        val temp=events[position]
+        temp.name=name
+        temp.location=location
+        temp.meetingInfo.replace("meetingAgenda",meeting_agenda)
+        temp.meetingInfo.replace("meetingMember",meeting_member)
+        eventsRef.document(events[position].id).set(temp)
     }
 
-//    fun edit(position: Int, name: String, location: String, startTime: String, endTime: String) {
-//        val event = events[position]
-//        event.name = name
-//        event.location = location
-//        event.startTime = startTime
-//        event.endTime = endTime
-//        eventsRef.document(events[position].id).set(event)
-//    }
+    fun editCourse(position: Int, name: String, location: String, keyContent: String, homework: String) {
+        val temp=events[position]
+        temp.name=name
+        temp.location=location
+        temp.courseInfo.replace("keyContent",keyContent)
+        temp.courseInfo.replace("homeWork",homework)
+        eventsRef.document(events[position].id).set(temp)
+    }
+
+    fun remove(position: Int) {
+        eventsRef.document(events[position].id).delete()
+    }
 
     override fun onBindViewHolder(holder: ScheduleViewHolder, position: Int) {
         holder.bind(events[position])
